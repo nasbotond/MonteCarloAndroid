56d50ae9819bfdf9be82a06459334ebe5f4dcbc4
Botond Nas <nas.botond@gmail.com>
2019-11-29 18:10:00 +0100

    add bottom navigation bar between fragments

--- app/src/main/java/com/kotlinforandroiddevelopment/montecarloandroid/MainActivity.kt
+++ app/src/main/java/com/kotlinforandroiddevelopment/montecarloandroid/MainActivity.kt
@@ -1,18 +1,40 @@
 package com.kotlinforandroiddevelopment.montecarloandroid
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: ActionBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // setSupportActionBar(toolbar)
+        // bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        /*
+        if (savedInstanceState == null) {
+            val fragment = PrintedDataFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+                .commit()
+        }
+        */
+
+        /*
         val runBtn =
             findViewById<Button>(R.id.runButton)
 
@@ -30,6 +52,8 @@ class MainActivity : AppCompatActivity() {
              */
             insertFragment(Integer.parseInt(findViewById<EditText>(R.id.numReps).text.toString()), Integer.parseInt(findViewById<EditText>(R.id.quota).text.toString()), Integer.parseInt(findViewById<EditText>(R.id.iterations).text.toString()))
         }
+
+         */
         /*
         val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.activity_main, ChartFragment.newInstance())
@@ -37,12 +61,44 @@ class MainActivity : AppCompatActivity() {
         */
     }
 
+    /*
     private fun insertFragment(numReps : Int, quota : Int, iterations : Int) {
         val transaction: FragmentTransaction =
             supportFragmentManager.beginTransaction()
+        val view = findViewById<ProgressBar>(R.id.loadingPanel)
+        view.visibility = View.GONE
         transaction.replace(R.id.activity_main, ChartFragment.newInstance(numReps, quota, iterations))
         transaction.addToBackStack(null)
         transaction.commit()
+    }
+
+     */
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+        when (menuItem.itemId) {
+            R.id.navigation_input -> {
+                toolbar.title = "Input"
+                val fragment = InputFragment.newInstance("", "")
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_chart -> {
+                toolbar.title = "Chart"
+                val fragment = ChartFragment.newInstance(0, 0, 0)
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_printedData -> {
+                toolbar.title = "Printed Data"
+                val fragment = PrintedDataFragment.newInstance("", "")
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 
 
