5be1b8f0d1d727f700fb46a7bde7e3ffa4da6ee8
Botond Nas <nas.botond@gmail.com>
2019-12-11 19:38:43 +0100

    add coroutine for calculation

--- app/src/main/java/com/kotlinforandroiddevelopment/montecarloandroid/InputFragment.kt
+++ app/src/main/java/com/kotlinforandroiddevelopment/montecarloandroid/InputFragment.kt
@@ -16,8 +16,9 @@ import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 import kotlin.collections.LinkedHashMap
+import kotlinx.coroutines.*
 
-class InputFragment : Fragment() {
+class InputFragment : Fragment(), CoroutineScope by MainScope() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,125 +49,138 @@ class InputFragment : Fragment() {
         val runBtn =
             view.findViewById<Button>(R.id.runButton)
 
-        runBtn.setOnClickListener{
+        view.findViewById<ProgressBar>(R.id.loadingPanel).visibility = View.INVISIBLE
 
-            view.findViewById<ProgressBar>(R.id.loadingPanel).visibility = View.VISIBLE
-            val dataset = calculatePoints(Integer.parseInt(view.findViewById<EditText>(R.id.numReps).text.toString()), Integer.parseInt(view.findViewById<EditText>(R.id.quota).text.toString()), Integer.parseInt(view.findViewById<EditText>(R.id.iterations).text.toString()))
-            view.findViewById<ProgressBar>(R.id.loadingPanel).visibility = View.INVISIBLE
-            listener.onFragmentSetDataset(dataset)
+        runBtn.setOnClickListener{
+            launch{
+
+                view.findViewById<ProgressBar>(R.id.loadingPanel).visibility = View.VISIBLE
+                val dataset = calculatePoints(Integer.parseInt(view.findViewById<EditText>(R.id.numReps).text.toString()),
+                    Integer.parseInt(view.findViewById<EditText>(R.id.quota).text.toString()),
+                    Integer.parseInt(view.findViewById<EditText>(R.id.iterations).text.toString()))
+
+                listener.onFragmentSetDataset(dataset)
+
+                view.findViewById<ProgressBar>(R.id.loadingPanel).visibility = View.INVISIBLE
+            }
             // insertFragment(Integer.parseInt(findViewById<EditText>(R.id.numReps).text.toString()), Integer.parseInt(findViewById<EditText>(R.id.quota).text.toString()), Integer.parseInt(findViewById<EditText>(R.id.iterations).text.toString()))
         }
         // Inflate the layout for this fragment
         return view
     }
 
-    private fun calculatePoints(reps: Int, quota: Int, iterations: Int) : DoubleArray {
-        val numCandidates: Int = reps
-        val quota: Int = quota
+    private suspend fun calculatePoints(reps: Int, quota: Int, iterations: Int) : DoubleArray {
 
-        val grid: MutableMap<Double, CoordinateWithPoints?> =
-            LinkedHashMap(numCandidates*numCandidates)
-        var averageTotalPivotalsPerIteration =
-            0.0 // [total pivotals] = averageTotalPivotalsPerIteration * [number of iterations]
-        // val start = System.currentTimeMillis()
-        val selected: MutableList<CoordinateWithPoints> =
-            ArrayList(numCandidates)
-        var numIterations = iterations
-        while (numIterations >= 0) {
-            numIterations -= 1
-            run {
-                var i = 0
-                while (i < numCandidates) {
-                    /*double randomX = normalRandomDoubleGenerator(mean, standardDeviation);
-                        double randomY = normalRandomDoubleGenerator(mean, standardDeviation);*/
-                    val randomX: Double = uniformRandomDoubleGenerator(0.0, 1.0)
-                    val randomY: Double = uniformRandomDoubleGenerator(0.0, 1.0)
-                    selected.add(CoordinateWithPoints(randomX, randomY))
-                    i += 1
+        return withContext(Dispatchers.Default){
+
+            val numCandidates: Int = reps
+            val quota: Int = quota
+
+            val grid: MutableMap<Double, CoordinateWithPoints?> =
+                LinkedHashMap(numCandidates*numCandidates)
+            var averageTotalPivotalsPerIteration =
+                0.0 // [total pivotals] = averageTotalPivotalsPerIteration * [number of iterations]
+            // val start = System.currentTimeMillis()
+            val selected: MutableList<CoordinateWithPoints> =
+                ArrayList(numCandidates)
+            var numIterations = iterations
+            while (numIterations >= 0) {
+                numIterations -= 1
+                run {
+                    var i = 0
+                    while (i < numCandidates) {
+                        /*double randomX = normalRandomDoubleGenerator(mean, standardDeviation);
+                            double randomY = normalRandomDoubleGenerator(mean, standardDeviation);*/
+                        val randomX: Double = uniformRandomDoubleGenerator(0.0, 1.0)
+                        val randomY: Double = uniformRandomDoubleGenerator(0.0, 1.0)
+                        selected.add(CoordinateWithPoints(randomX, randomY))
+                        i += 1
+                    }
                 }
-            }
-            val sortedByY: List<CoordinateWithPoints> =
-                ArrayList(selected)
-            RecCounter.sortByX(selected)
-            RecCounter.sortByY(sortedByY)
-            try {
-                val iterationTotalPivotals = RecCounter.count(
-                    selected,
-                    true,
-                    quota
-                ) // adds points to those on the edge of non-reducible rectangles: (NOTE) for example, if a non-reducible rectangle has eight on the edge, two on each of the four sides, points will be given even if the rectangle quota is one over the acceptable quota
-                averageTotalPivotalsPerIteration =
-                    averageTotalPivotalsPerIteration - averageTotalPivotalsPerIteration / numIterations + iterationTotalPivotals / numIterations // (averageTotalPivotalsPerIteration * (numIterations - 1) + iterationTotalPivotals) / numIterations;
-            } catch (exception: Exception) {
-                exception.printStackTrace()
-            }
-            // map selected coordinates to grid
+                val sortedByY: List<CoordinateWithPoints> =
+                    ArrayList(selected)
+                RecCounter.sortByX(selected)
+                RecCounter.sortByY(sortedByY)
+                try {
+                    val iterationTotalPivotals = RecCounter.count(
+                        selected,
+                        true,
+                        quota
+                    ) // adds points to those on the edge of non-reducible rectangles: (NOTE) for example, if a non-reducible rectangle has eight on the edge, two on each of the four sides, points will be given even if the rectangle quota is one over the acceptable quota
+                    averageTotalPivotalsPerIteration =
+                        averageTotalPivotalsPerIteration - averageTotalPivotalsPerIteration / numIterations + iterationTotalPivotals / numIterations // (averageTotalPivotalsPerIteration * (numIterations - 1) + iterationTotalPivotals) / numIterations;
+                } catch (exception: Exception) {
+                    exception.printStackTrace()
+                }
+                // map selected coordinates to grid
 //
-            val mockGrid: MutableMap<CoordinateWithPoints, Int> =
-                HashMap(numCandidates)
-            run {
+                val mockGrid: MutableMap<CoordinateWithPoints, Int> =
+                    HashMap(numCandidates)
+                run {
+                    var i = 0
+                    while (i < selected.size) {
+                        val key = selected[i]
+                        mockGrid[key] = i
+                        i += 1
+                    }
+                }
                 var i = 0
-                while (i < selected.size) {
-                    val key = selected[i]
-                    mockGrid[key] = i
+                while (i < sortedByY.size) {
+                    val x = mockGrid[sortedByY[i]]!!
+                    val y = i
+                    val points = sortedByY[i].points
+                    val key: Double = generateGridIntegerKey(x, y, numCandidates)
+                    if (!grid.containsKey(key)) {
+                        grid[key] = CoordinateWithPoints(x.toDouble(), y.toDouble(), points)
+                    } else {
+                        grid[key]!!.incrementPoints(points)
+                    }
                     i += 1
                 }
+                //
+                // setInput(setValueForChart(grid, numIterations, averageTotalPivotalsPerIteration));
+                selected.clear()
             }
-            var i = 0
-            while (i < sortedByY.size) {
-                val x = mockGrid[sortedByY[i]]!!
-                val y = i
-                val points = sortedByY[i].points
-                val key: Double = generateGridIntegerKey(x, y, numCandidates)
-                if (!grid.containsKey(key)) {
-                    grid[key] = CoordinateWithPoints(x.toDouble(), y.toDouble(), points)
-                } else {
-                    grid[key]!!.incrementPoints(points)
+            // val end = System.currentTimeMillis()
+
+            // val arrayLength: Int = grid.size
+            // val xs = DoubleArray(arrayLength)
+            // val ys = DoubleArray(arrayLength)
+            // val points = DoubleArray(arrayLength)
+            val returns = DoubleArray(reps)
+
+            var index = 0
+            for ((_, value) in grid)  // update Banzhaf indices all coordinates in the grid, and update dataset
+            {
+                val coordinateTotalPivotals = value?.points
+                if (coordinateTotalPivotals != null) {
+                    value?.value = coordinateTotalPivotals.toDouble() / numIterations / averageTotalPivotalsPerIteration
                 }
+                /*
+                if (value != null) {
+                    xs[index] = value.x
+                }
+                if (value != null) {
+                    ys[index] = value.y
+                }
+
+                 */
+                if (value != null) {
+                    // points[index] = value.value
+                    returns[value.x.toInt()] = returns[value.x.toInt()].plus(value.value)
+                }
+                index += 1
+            }
+
+            var i = 0
+            while (i < returns.size) {
+                returns[i] = returns[i] / returns.size
                 i += 1
             }
-            //
-            // setInput(setValueForChart(grid, numIterations, averageTotalPivotalsPerIteration));
-            selected.clear()
-        }
-        // val end = System.currentTimeMillis()
+            return@withContext returns
 
-        // val arrayLength: Int = grid.size
-        // val xs = DoubleArray(arrayLength)
-        // val ys = DoubleArray(arrayLength)
-        // val points = DoubleArray(arrayLength)
-        val returns = DoubleArray(reps)
-
-        var index = 0
-        for ((_, value) in grid)  // update Banzhaf indices all coordinates in the grid, and update dataset
-        {
-            val coordinateTotalPivotals = value?.points
-            if (coordinateTotalPivotals != null) {
-                value?.value = coordinateTotalPivotals.toDouble() / numIterations / averageTotalPivotalsPerIteration
-            }
-            /*
-            if (value != null) {
-                xs[index] = value.x
-            }
-            if (value != null) {
-                ys[index] = value.y
-            }
-
-             */
-            if (value != null) {
-                // points[index] = value.value
-                returns[value.x.toInt()] = returns[value.x.toInt()].plus(value.value)
-            }
-            index += 1
         }
 
-        var i = 0
-        while (i < returns.size) {
-            returns[i] = returns[i] / returns.size
-            i += 1
-        }
-
-        return returns
     }
 
     private fun uniformRandomDoubleGenerator(
