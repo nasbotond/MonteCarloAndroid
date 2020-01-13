70cf59b8bdceca62792f920159a14c151574687b
nasbotond <nas.botond@gmail.com>
2020-01-13 10:18:38 +0100

    add equals and hashCode functions to data class

--- app/src/main/java/com/kotlinforandroiddevelopment/montecarloandroid/Data.kt
+++ app/src/main/java/com/kotlinforandroiddevelopment/montecarloandroid/Data.kt
@@ -1,4 +1,18 @@
 package com.kotlinforandroiddevelopment.montecarloandroid
 
 data class Data(var data : DoubleArray) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Data
+
+        if (!data.contentEquals(other.data)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return data.contentHashCode()
+    }
 }
